eb16bf57db7f01405bf21703ed407da625005981
Wontai <ds1adr@gmail.com>
2026-09-16 23:50:46 -0700

    Update View (Activity)

--- app/src/main/java/com/ds2adr/composeviewmodelexample/MainActivity.kt
+++ app/src/main/java/com/ds2adr/composeviewmodelexample/MainActivity.kt
@@ -1,47 +1,60 @@
 package com.ds2adr.composeviewmodelexample
 
+import android.R.attr.name
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ds2adr.composeviewmodelexample.ui.theme.ComposeViewModelExampleTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: TracksViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComposeViewModelExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TrackListView(
+                        viewModel = viewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
+        lifecycleScope.launch {
+            viewModel.loadTracks()
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TrackListView(viewModel: TracksViewModel, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(viewModel.tracks, key = { it.id }) { track ->
+            Text(text = track.name)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeViewModelExampleTheme {
-        Greeting("Android")
+        TrackListView(viewModel = viewModel(), modifier = Modifier.fillMaxSize())
     }
 }
